b3ce9fd7c8ccf3d5432dda47b489ed5353ac4f32
Hanna Grehn <hannagrehn@gmail.com>
2023-12-21 17:02:15 +0100

    ewwww

--- MauiPhonebook/Views/ContactsPage.xaml.cs
+++ MauiPhonebook/Views/ContactsPage.xaml.cs
@@ -5,15 +5,27 @@ public partial class ContactsPage : ContentPage
 	public ContactsPage()
 	{
 		InitializeComponent();
+
+		List<Contact> contacts = new List<Contact>()
+		{
+			new Contact { FirstName = "Frank", LastName = "Hanks", Email = "frank@hans.com" },
+            new Contact { FirstName = "Hank", LastName = "Hanks", Email = "hanks@hans.com" },
+            new Contact { FirstName = "Mank", LastName = "Toms", Email = "mank@toms.se" }
+		};
+
+		listContacts.ItemsSource = contacts;
+
+			
 	}
 
-    private void btnAddContact_Clicked(object sender, EventArgs e)
-    {
-        Shell.Current.GoToAsync(nameof(AddContactPage));
-    }
+	public class Contact
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
 
-    private void btnEditContact_Clicked(object sender, EventArgs e)
-    {
-        Shell.Current.GoToAsync(nameof(EditContactPage));
-    }
+
+	}
+
+
 }
