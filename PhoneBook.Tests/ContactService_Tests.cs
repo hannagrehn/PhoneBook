52d0e83c3d93cf58a980a42c7840e2ce70d310ab
Hanna Grehn <hannagrehn@gmail.com>
2023-12-29 14:49:54 +0100

    .

--- PhoneBook.Tests/ContactService_Tests.cs
+++ PhoneBook.Tests/ContactService_Tests.cs
@@ -11,14 +11,27 @@ public class ContactService_Tests
 
     public void AddContactToList_Should_Add_OneContactToList_ReturnTrue()
     {
-        IContact Contact = new Contact { FirstName = "Dolly", LastName = "Pardon", Email = "dolly@pardon.com" };
+        IContact contact = new Contact { FirstName = "Dolly", LastName = "Pardon", Email = "dolly@pardon.com" };
 
         IContactService contactService = new ContactService();
 
-        bool result = contactService.AddContactToList(Contact);
+        bool result = contactService.AddContactToList(contact);
 
         Assert.True(result);
 
 
+    }
+
+    [Fact]
+    public void GetAllContactsFromListShould_GetAllContactsInContactList_ReturnListOfContacts()
+    {
+        IContactService contactService = new ContactService();
+        IContact contact = new Contact { FirstName = "Dolly", LastName = "Pardon", Email = "dolly@pardon.com" };
+        contactService.AddContactToList(contact);
+
+        var result = contactService.GetAllFromList();
+
+        Assert.NotNull(result);  
+        Assert.True(((IEnumerable<Contact>)result).Any());
     }
 }
